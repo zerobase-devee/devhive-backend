1f194fc014067b95f7816a95d6a862c08c0dda59
Ameri-Kano <81066717+Ameri-Kano@users.noreply.github.com>
2023-08-11 22:16:01 +0900

    INIT - 프로젝트 초기 설정

---
+++ src/main/java/com/devee/devhive/DevHiveApplication.java
@@ -0,0 +1,13 @@
+package com.devee.devhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devee/devhive/DevHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devee.devhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
