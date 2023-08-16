2a036cd12908e98fc64374d5d7219a3918511d74
JaeYooooon <dhy04050@naver.com>
2023-08-16 13:03:41 +0900

    fix : 유저 회원가입 1. 유저 회원가입 시 비밀번호 암호화, 상태 활성화 2. 회원가입 시 등록일자 안들어가서 @EnableJpaAuditing 작성

--- src/main/java/com/devee/devhive/DevHiveApplication.java
+++ src/main/java/com/devee/devhive/DevHiveApplication.java
@@ -2,12 +2,14 @@ package com.devee.devhive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DevHiveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevHiveApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DevHiveApplication.class, args);
+  }
 
 }
--- src/main/java/com/devee/devhive/domain/auth/service/AuthService.java
+++ src/main/java/com/devee/devhive/domain/auth/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.devee.devhive.domain.auth.service;
 
+import static com.devee.devhive.domain.user.type.ActivityStatus.ACTIVITY;
 import static com.devee.devhive.global.exception.ErrorCode.DUPLICATE_EMAIL;
 import static com.devee.devhive.global.exception.ErrorCode.DUPLICATE_NICKNAME;
 import static com.devee.devhive.global.exception.ErrorCode.EXPIRED_VERIFY_CODE;
@@ -11,8 +12,11 @@ import com.devee.devhive.domain.auth.service.mail.MailService;
 import com.devee.devhive.domain.user.entity.User;
 import com.devee.devhive.domain.user.repository.UserRepository;
 import com.devee.devhive.global.exception.CustomException;
+import com.devee.devhive.global.security.JwtTokenProvider;
+import com.devee.devhive.global.security.UserDetailsServiceImpl;
 import com.devee.devhive.global.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +27,7 @@ public class AuthService {
   private final UserRepository userRepository;
   private final MailService mailService;
   private final RedisUtil redisUtil;
+  private final PasswordEncoder passwordEncoder;
 
   // 인증 코드
   public void getVerificationCode(EmailDTO emailDTO) throws Exception {
@@ -44,8 +49,9 @@ public class AuthService {
     User user = User.builder()
         .region(joinDTO.getRegion())
         .email(joinDTO.getEmail())
-        .password(joinDTO.getPassword())
+        .password(passwordEncoder.encode(joinDTO.getPassword()))
         .nickName(joinDTO.getNickName())
+        .status(ACTIVITY)
         .build();
 
     String enteredCode = joinDTO.getVerificationCode();
