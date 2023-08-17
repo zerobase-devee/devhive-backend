016dae8cb56ea12de45fe8d39d85f8deda3a0af6
JaeYooooon <dhy04050@naver.com>
2023-08-17 21:01:49 +0900

    fix : toEntity() 1. Role.GUEST -> Role.USER 수정

--- src/main/java/com/devee/devhive/global/oauth2/domain/OAuthAttributes.java
+++ src/main/java/com/devee/devhive/global/oauth2/domain/OAuthAttributes.java
@@ -66,7 +66,7 @@ public class OAuthAttributes {
         .email(email)
         .profileImage(profileImage)
         .password("OAUTH_TEST")
-        .role(Role.GUEST)
+        .role(Role.USER)
         .build();
   }
 }
