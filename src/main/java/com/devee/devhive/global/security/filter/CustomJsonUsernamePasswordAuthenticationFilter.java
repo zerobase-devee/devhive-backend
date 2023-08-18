0a4f98f987ab2f620f2e5ffb6b0a64acab0aa269
JaeYooooon <dhy04050@naver.com>
2023-08-19 01:15:01 +0900

    fix : attemptAuthentication 1. attemptAuthentication 메서드 내의 인증 방식을 LoginDto 를 사용하는 방식으로 수정

--- src/main/java/com/devee/devhive/global/security/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/devee/devhive/global/security/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
@@ -1,18 +1,16 @@
 package com.devee.devhive.global.security.filter;
 
+import com.devee.devhive.domain.auth.dto.LoginDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.util.StreamUtils;
 
 //로그인 처리 필터
 public class CustomJsonUsernamePasswordAuthenticationFilter extends
@@ -21,8 +19,6 @@ public class CustomJsonUsernamePasswordAuthenticationFilter extends
   private static final String DEFAULT_LOGIN_REQUEST_URL = "/api/auth/signin"; // "/v1/auth/signin"으로 오는 요청을 처리
   private static final String HTTP_METHOD = "POST"; // 로그인 HTTP 메소드는 POST
   private static final String CONTENT_TYPE = "application/json"; // JSON 타입의 데이터로 오는 로그인 요청만 처리
-  private static final String USERNAME_KEY = "email"; // 회원 로그인 시 이메일 요청 JSON Key : "email"
-  private static final String PASSWORD_KEY = "password"; // 회원 로그인 시 비밀번호 요청 JSon Key : "password"
   private static final AntPathRequestMatcher DEFAULT_LOGIN_PATH_REQUEST_MATCHER =
       new AntPathRequestMatcher(DEFAULT_LOGIN_REQUEST_URL,
           HTTP_METHOD);
@@ -43,17 +39,27 @@ public class CustomJsonUsernamePasswordAuthenticationFilter extends
       throw new AuthenticationServiceException(
           "Authentication Content-Type not supported: " + request.getContentType());
     }
+//    String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+//
+//    var usernamePasswordMap = objectMapper.readValue(messageBody, Map.class);
+//
+//    Object email = usernamePasswordMap.get(USERNAME_KEY);
+//    Object password = usernamePasswordMap.get(PASSWORD_KEY);
+//
+//    UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(email,
+//        password);//principal 과 credentials 전달
+//
+//    return this.getAuthenticationManager().authenticate(authRequest);
 
-    String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+    // LoginDto 객체로 매핑
+    ObjectMapper om = new ObjectMapper();
+    LoginDto loginDto = om.readValue(request.getInputStream(), LoginDto.class);
+    // 인증 정보를 사용하여 UsernamePasswordAuthenticationToken 객체 생성
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+        loginDto.getEmail(),
+        loginDto.getPassword());
 
-    var usernamePasswordMap = objectMapper.readValue(messageBody, Map.class);
-
-    Object email = usernamePasswordMap.get(USERNAME_KEY);
-    Object password = usernamePasswordMap.get(PASSWORD_KEY);
-
-    UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(email,
-        password);//principal 과 credentials 전달
-
-    return this.getAuthenticationManager().authenticate(authRequest);
+    // 인증 처리 시작 -> 결과 반환
+    return getAuthenticationManager().authenticate(authenticationToken);
   }
 }
