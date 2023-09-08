7d42dae9138dd43e51b21826fc41c28b5b3a1357
Ameri-Kano <yeokkim13@naver.com>
2023-09-08 14:21:44 +0900

    refactor : ChatMessageDto 클래스 멤버 간략화

--- src/main/java/com/devee/devhive/domain/project/chat/entity/dto/ChatMessageDto.java
+++ src/main/java/com/devee/devhive/domain/project/chat/entity/dto/ChatMessageDto.java
@@ -2,7 +2,6 @@ package com.devee.devhive.domain.project.chat.entity.dto;
 
 
 import com.devee.devhive.domain.project.chat.entity.ProjectChatMessage;
-import com.devee.devhive.domain.project.type.ChatMessageType;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,19 +16,15 @@ import lombok.Setter;
 @Builder
 public class ChatMessageDto {
 
-  private Long chatRoomId;
   private Long userId;  // 보낸 유저
   private String text;
   private LocalDateTime sendTime;
-  private ChatMessageType messageType;
 
   public static ChatMessageDto from(ProjectChatMessage message) {
     return ChatMessageDto.builder()
-        .chatRoomId(message.getProjectChatRoom().getId())
         .userId(message.getSenderUser().getId())
         .text(message.getText())
         .sendTime(message.getCreatedDate())
-        .messageType(message.getMessageType())
         .build();
   }
 }
