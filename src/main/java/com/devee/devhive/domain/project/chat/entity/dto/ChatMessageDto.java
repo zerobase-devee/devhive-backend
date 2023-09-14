d4eaf6e780a5f9e87001fa50db8e08592517b0c8
mingkiii <kimchunjae92@gmail.com>
2023-09-14 17:36:39 +0900

    fix : ChatMessageDto 수정

--- src/main/java/com/devee/devhive/domain/project/chat/entity/dto/ChatMessageDto.java
+++ src/main/java/com/devee/devhive/domain/project/chat/entity/dto/ChatMessageDto.java
@@ -2,6 +2,7 @@ package com.devee.devhive.domain.project.chat.entity.dto;
 
 
 import com.devee.devhive.domain.project.chat.entity.ProjectChatMessage;
+import com.devee.devhive.domain.user.entity.User;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,12 +18,15 @@ import lombok.Setter;
 public class ChatMessageDto {
 
   private Long userId;  // 보낸 유저
+  private String userNickname;
   private String text;
   private LocalDateTime sendTime;
 
   public static ChatMessageDto from(ProjectChatMessage message) {
+    User senderUser = message.getSenderUser();
     return ChatMessageDto.builder()
-        .userId(message.getSenderUser().getId())
+        .userId(senderUser.getId())
+        .userNickname(senderUser.getNickName())
         .text(message.getText())
         .sendTime(message.getCreatedDate())
         .build();
